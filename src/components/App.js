99b236d0607db009e223ba251a82f36c77ba4fed
Isaiah Santala <Isantala@yahoo.com>
2020-03-15 12:40:20 -0700

    adds gallery and modal

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,51 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
-import Image from './Image'
+import Gallery from "react-photo-gallery"
 
 const App = () => {
-  const [images, setImages] = useState([])
-
-  const getImages = async () => {
-    const { data } = await axios('/images')
-    setImages(data)
-  }
-
+  const [images, setImages] = useState([{ src: null, height: null, width: null }])
   useEffect(() => {
+    const getImages = async () => {
+      const { data } = await axios('/images')
+      setImages(data)
+    } 
     getImages()
   }, [])
+
+  const [modalImageSrc, setModalImageSrc] = useState('')
+  const [modelIsOpen, setModalIsOpen] = useState(false)
+
+  const openModal = ({src}) => {
+    setModalImageSrc(src)
+    setModalIsOpen(true)
+  }
+
+  const closeModal = () => {
+    setModalImageSrc('')
+    setModalIsOpen(false)
+  }
 
   return (
     <div>
       <div className="nav">
         <div className="nav-logo">SIMONANDREW</div>
       </div>
-      <div className="images">
-        {images.map((imageSrc, i) => (
-          <Image key={i} imageSrc={imageSrc}/>
-        ))}
-      </div>
+
+      <Gallery 
+        photos={images} 
+        targetRowHeight={500} 
+        margin={0}
+        onClick={({target}) => openModal(target)}
+      />
+
+      {modelIsOpen && 
+        <div className="modal">
+          <div className="modal-content">
+            <img className="image" src={modalImageSrc}></img>
+            <button>Purchase</button>
+          </div>
+        </div>
+      }
     </div>
   )
 }
--- src/components/Image.js
+++ src/components/Image.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Image = ({imageSrc}) => {
   return (
     <div className="image" style={{ backgroundImage: `url(${imageSrc})`}}>
-      <button>buy</button>
+      <button></button>
     </div>
   )
 }
